bc9cba624a0bb54231dad5a85f373d1827d6f8ac
Anthony Smith <anthonysmith@anthonys-imac.lan>
2021-02-24 06:24:17 +0000

    feature: Add hom link to header icon

--- frontend/src/molecules/header/Header.tsx
+++ frontend/src/molecules/header/Header.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Navigation from "../navigation/Navigation";
+import { Link } from "react-router-dom";
 
 import BrandIcon from "../../atoms/brand-icon/BrandIcon";
 import navigation from "../../constants/navigation";
@@ -8,8 +9,12 @@ import navigation from "../../constants/navigation";
 const Header = () => {
   const theme = useTheme();
   return (
-    <Box height={60} boxShadow={1} padding={2} display="flex" alignItems="center" justifyContent="space-between">
-      <BrandIcon color={theme.palette.primary.main} />
+    <Box boxShadow={1} padding={2} display="flex" alignItems="center" justifyContent="space-between">
+      <Link to="/">
+        <Box height={60}>
+          <BrandIcon color={theme.palette.primary.main} />
+        </Box>
+      </Link>
       <Navigation navigation={navigation} />
     </Box>
   );
