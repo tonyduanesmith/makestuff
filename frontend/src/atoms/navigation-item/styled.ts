bd5847a73a291c46d271b6f8d2bdf13d658f5bfb
Anthony Smith <anthonysmith@anthonys-imac.lan>
2021-02-25 20:39:07 +0000

    feature: Add hiver on nav item

--- frontend/src/atoms/navigation-item/styled.ts
+++ frontend/src/atoms/navigation-item/styled.ts
@@ -4,4 +4,9 @@ import { Link } from "react-router-dom";
 export const StyledLink = styled(Link)`
   text-decoration: none;
   color: ${p => p.theme.palette.common.black};
+  transition: color 100ms ease-in;
+
+  &:hover {
+    color: ${p => p.theme.palette.grey[700]};
+  }
 `;
