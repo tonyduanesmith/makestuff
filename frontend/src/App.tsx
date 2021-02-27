36f9bb2cfcd4597ef8c7ff6e884193ffff2148cb
Anthony Smith <tonyduanesmith@me.com>
2021-02-27 23:00:55 +0000

    feature: Add apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import { ThemeProvider } from "styled-components";
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider, NormalizedCacheObject } from "@apollo/client";
 
 import theme from "./theme";
 import Routes from "./routes/Routes";
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000",
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink,
+});
+
 const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <Routes />
-      </ThemeProvider>
-    </MuiThemeProvider>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </ApolloProvider>
   );
 };
 
